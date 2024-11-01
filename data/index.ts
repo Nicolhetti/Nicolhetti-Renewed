72b9221a4925d95b7a0559b2f6fff742574a5937
Nicolhetti <143234675+Nicolhetti@users.noreply.github.com>
2024-11-01 20:28:40 -0300

    Thanks 👌

--- data/index.ts
+++ data/index.ts
@@ -35,9 +35,9 @@ export const aboutPage = {
   Todo esto lleva mucho tiempo y dedicación, ya que soy una sola persona comprometida con brindarles la mejor experiencia posible.<br><br>
   Mi objetivo es que puedan disfrutar de los juegos sin preocupaciones, sabiendo que han sido revisados para su seguridad.<br><br>
   Espero que valoren el esfuerzo que pongo en cada descarga y que juntos podamos construir una comunidad más segura para todos los gamers.<br><br>
-  ¡Gracias por formar parte de esta aventura!
+  ¡Gracias por formar parte de esta aventura!<br><br><br>
   
-  Y muchas gracias a <a href="https://github.com/goodendinggg"> <span class="px-3">goodendinggg</span></a> por la ayuda con la web 👌`,
+  Y muchas gracias a <a href="https://github.com/goodendinggg" class="text-yellow-400 hover:text-yellow-300 font-bold transition duration-300"><span class="px-3 animate-pulse">goodendinggg</span></a> por la ayuda con la web 👌`,
 }
 
 export const seoData = {
