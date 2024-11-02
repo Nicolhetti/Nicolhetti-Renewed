6fcbac1e41f84b570e71c82f5b1e7afdc2b120f5
nicolasimanolmedina <nicolasimanolmedina@gmail.com>
2024-11-02 07:19:37 +0000

    FUCK :*

--- components/archive/card.vue
+++ components/archive/card.vue
@@ -29,8 +29,8 @@ withDefaults(defineProps<Props>(), {
     <NuxtLink :to="path" class="grid grid-cols-1 sm:grid-cols-10 gap-1">
       <div class="sm:col-span-3">
         <NuxtImg
-          class="image-fixed-height"
-          width="400" height="300"
+          class="h-full w-full object-cover object-center rounded-t-2xl sm:rounded-l-2xl sm:rounded-t-none shadow-lg group-hover:scale-[1.02] transition-all duration-500 image-fixed-height"
+          width="300"
           :src="image"
           :alt="alt"
         />
