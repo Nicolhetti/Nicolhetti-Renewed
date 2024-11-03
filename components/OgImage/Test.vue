d727671973920620e49be0337a949a78b60987a1
nicolasimanolmedina <nicolasimanolmedina@gmail.com>
2024-11-03 20:29:22 +0000

    Test OG Image size

--- components/OgImage/Test.vue
+++ components/OgImage/Test.vue
@@ -71,7 +71,7 @@ const formattedLink = computed(() => {
     </div>
 
     <div v-if="formattedLink" class="absolute top-[200px] right-[70px]">
-      <img :src="formattedLink" :alt="title" class="w-96 h-52 rounded-lg object-cover">
+      <img :src="formattedLink" :alt="title" class="w-52 h-96 rounded-lg object-cover">
     </div>
   </div>
 </template>
